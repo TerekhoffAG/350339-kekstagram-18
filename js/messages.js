62ac008e324b879a212dd58ced56ab31a7f9bf72
Terekhov_A <terekhoffag@gmail.com>
2019-10-14 04:58:33 +0400

    Личный проект: воздушные формы

--- js/messages.js
+++ js/messages.js
@@ -1,10 +1,11 @@
 'use strict';
 (function () {
   var mainBlock = document.querySelector('main');
-  var templateMessage = document.querySelector('#error').content.querySelector('.error');
+  var templateError = document.querySelector('#error').content.querySelector('.error');
+  var templateSuccess = document.querySelector('#success').content.querySelector('.success');
 
   window.openPopupError = function () {
-    var popupError = templateMessage.cloneNode(true);
+    var popupError = templateError.cloneNode(true);
     mainBlock.appendChild(popupError);
 
     var closePopup = function () {
@@ -34,6 +35,41 @@
     };
 
     popupError.addEventListener('click', onButtonClick);
+    document.addEventListener('click', onSectionClick);
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  window.openPopupSuccess = function () {
+    var popupSuccess = templateSuccess.cloneNode(true);
+    mainBlock.appendChild(popupSuccess);
+
+    var closePopup = function () {
+      popupSuccess.remove();
+
+      popupSuccess.removeEventListener('click', onButtonClick);
+      document.removeEventListener('click', onSectionClick);
+      document.removeEventListener('keydown', onPopupEscPress);
+    };
+
+    var onButtonClick = function (evt) {
+      if (evt.target.nodeName === 'BUTTON') {
+        closePopup();
+      }
+    };
+
+    var onSectionClick = function (evt) {
+      if (evt.target.nodeName === 'SECTION') {
+        closePopup();
+      }
+    };
+
+    var onPopupEscPress = function (evt) {
+      if (evt.keyCode === window.util.ESC_KEYCODE) {
+        closePopup();
+      }
+    };
+
+    popupSuccess.addEventListener('click', onButtonClick);
     document.addEventListener('click', onSectionClick);
     document.addEventListener('keydown', onPopupEscPress);
   };
